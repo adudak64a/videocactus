72af08d36704b1f2710bcaa647a2ff00abf18a7e
adudak <adudak64a@gmail.com>
2023-04-14 16:00:44 +0300

    added python check script

---
+++ jenkins_check.py
@@ -0,0 +1,22 @@
+from subprocess import Popen
+from signal import SIGTERM
+from time import time, sleep
+from os import kill
+
+# Define the command to run and its arguments
+command = ['python3', 'telegram_bot_videocactus.py']
+
+# Define the timeout in seconds
+timeout = 10
+
+# Start the subprocess and get its PID
+process = Popen(command)
+
+# Wait for the subprocess to complete or for the timeout to expire
+start_time = time()
+while process.poll() is None and time() - start_time < timeout:
+    sleep(0.1)
+
+# If the subprocess is still running, terminate it
+if process.poll() is None:
+    kill(process.pid, SIGTERM)
